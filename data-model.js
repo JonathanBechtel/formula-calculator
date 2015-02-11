02bea41aa392ed5787fa610693bfef0ab806a1f0
JonathanBechtel <JonathanBechtel@gmail.com>
2015-02-10 21:23:39 -0500

    Update data-model.js
    Created additional math functions for sections 1-5 (up to finances section).  
    
    Other notes:  
    
    I changed the app function because when I tried to test it I couldn't get it to run correctly.  
    
    I also had a difficult time using functions that stored arrays locally, so instead I made the arrays global, put info inside them and then used them for future calculations.
    
    Regarding the last point, I always had an intuition that this was not the best way to do it, but couldn't figure out an easier way to get it done so I kept doing it.  However, I did the formulas for calculating price last, and I used a slightly different method that didn't require me to keep making new global arrays.  Would be interested in hearing your opinions on how to do this.  
    
    I realize some of the changes I made to your code might not be optimal, but I wanted to be able to work on formulas that I could get to work and have you critique them.

--- data-model.js
+++ data-model.js
@@ -1,20 +1,202 @@
-var app = (function() {
+	//variables that'll be saved from form in first part of form
+	servingsPerBottle = 30;
+	bottlesPerRun = 1500;
+	
 	//array that'll hold all ingredients w/in formula
 	var formula = [];
-	var carrierPerServing = function(ingredient) {
+	//array that'll hold carrier amounts w/in formula
+	var carrierServingArr = [];
+	//array that'll hold total ingredient amounts per serving w/in formula
+	var totalServingArr = [];
+	//array that'll hold total amount of carrier for each ingredient in a single bottle
+	var totalCarrierBottleArr = [];
+	//array that'll hold total amount of each ingredient in a bottle, carrier + actives included
+	var totalServingBottleArr = [];
+	//array that'll hold total amount of carrier for each ingredient in a run
+	var totalCarrierRunArr = [];
+	//array that'll hold total amount of each ingredient in a run, carrier + actives included
+	var totalServingRunArr = [];
+	
+	//calculates amount of carrier in each serving of product
+	var carrierPerIngredient = function(ingredient) {
 		return (ingredient.carrier/100)*(ingredient.amount/(1-(ingredient.carrier/100)));
 	};
-
-	return {
-		listCarrierPerServing: function() {
-			var returnArr = [];
+	
+	// ------ Formulas for Section 3 ----------------- //
+	
+	//returns array of carrier amounts for each ingredient on a per serving basis
+	var	listCarrierPerServing = function() {
 			for (var i = 0; i < formula.length; i++) {
-				returnArr.push(carrierPerServing(formula[i]));
+				carrierServingArr.push(carrierPerIngredient(formula[i]));
 			}
-			return returnArr;
-		},
+	};
+	
+	//calculates total amount for each ingredient in serving
+	var servingTotalPerIngredient = function(ingredient) {
+		return carrierServingArr[ingredient] + formula[ingredient].amount;
+	};
+	
+	//returns array of total ingredient amounts for each ingredient
+	var listServingTotalAmount = function() {
+		for (var i = 0; i < formula.length; i++) {
+			totalServingArr.push(servingTotalPerIngredient(i));
+		}
+	};
+	
+	//calculates total carrier amount for individual serving by adding together values in carrierServingArr
+	var carrierPerServing = function() {
+		return carrierServingArr.reduce(function (i, c) {
+			return i + c
+		});
+	};
+	
+	//calculates total amount of actives in a serving 
+	var totalActivesServing = function() {
+		var activesArr = [];
+		for (var i = 0; i < formula.length; i++) {
+			activesArr.push(formula[i].amount);
+		}
+		
+		return activesArr.reduce(function (i, c) {
+			return i + c
+		});
+	};
+	
+	//calculates total ingredient amount for a serving, carrier + actives included
+	var amountPerServing = function() {
+		return totalServingArr.reduce(function (i, c) {
+			return i + c
+		});
+	};
+	
+	// ------------- Formulas for Section 4 ------------------------- //
+	
+	//calculates total amount of carrier for an ingredient in a single bottle.
+	var carrierAmtPerBottle = function(ingredient) {
+		return carrierServingArr[ingredient] * servingsPerBottle;
+	};
+	
+	//stores carrier/bottle amount in an array
+	var listCarrierPerBottle = function() {
+		for (var i = 0; i < formula.length; i++) {
+			totalCarrierBottleArr.push(carrierAmtPerBottle(i));
+		}
+	};
+	
+	//calculates total ingredient amt per bottle
+	var servingTotalPerBottle = function(ingredient) {
+		return totalCarrierBottleArr[ingredient] + (formula[ingredient].amount * servingsPerBottle);
+	};
+	
+	//stores per bottle serving totals for each ingredient in an array
+	var listTotalServingPerBottle = function() {
+		for (var i = 0; i < formula.length; i++) {
+			totalServingBottleArr.push(servingTotalPerBottle(i));
+		}
+	};
+	
+	// calculates price of each ingredient in the bottle
+	var priceIngredientBottle = function(i) {
+		return (totalServingBottleArr[i]/1000000) * formula[i].usdperkilo;
+	};
+	
+	//adds up values of carrier/bottle of each ingredient
+	var carrierPerBottle = function() {
+		return totalCarrierBottleArr.reduce(function (i, c) {
+			return i + c
+		});
+	};
+	
+	//calculates total amount of actives in a serving 
+	var totalActivesBottle = function() {
+		return totalActivesServing() * servingsPerBottle;
+	};
+	
+	//adds up values of total amount of each ingredient per bottle
+	var totalPerBottle = function() {
+		return totalServingBottleArr.reduce(function (i, c) {
+			return i + c
+		});
+	};
+	
+	//sums up the total price of the bottle ** - I didn't create an additional array to do this, unlike other functions.  Is this better? 
+	var totalPriceBottle = function() {
+		tempPriceArray = [];
+		for (var i = 0; i < formula.length; i++) {
+			tempPriceArray.push(priceIngredientBottle(i));
+		}
+		
+		return tempPriceArray.reduce(function (i, c) {
+			return i + c
+		});
+	}
+	
+	// --------------- Formulas for Section 5 ---------------------- //
+	
+	//calculates amount of carrier for each ingredient in a run
+	var carrierPerRun = function(ingredient) {
+		return carrierAmtPerBottle(ingredient) * bottlesPerRun;
+	};
+	
+	//stores values from carrierPerRun in an array
+	var listCarrierPerRun = function() {
+		for (var i = 0; i < formula.length; i++) {
+			totalCarrierRunArr.push(carrierPerRun(i));
+		}
+	};
+	
+	//calculates total amount needed for each ingredient in an entire run
+	var totalPerRun = function(ingredient) {
+		return totalCarrierRunArr[ingredient] + (formula[ingredient].amount * servingsPerBottle * bottlesPerRun);
+	};
+	
+	//stores values for totalPerRun for each ingredient in an array
+	var listTotalPerRun = function() {
+		for (var i = 0; i < formula.length; i++) {
+			totalServingRunArr.push(totalPerRun(i));
+		}
+	};
+	
+	//calculates total price of an ingredient for an entire run
+	var priceIngredientRun = function(i) {
+		return (totalServingBottleArr[i]/1000000) * formula[i].usdperkilo * bottlesPerRun;
+	};
+	
+	//adds up values in totalCarrierRunArr, giving a total amount of carrier used for the entire run
+	var totalCarrierPerRun = function() {
+		return totalCarrierRunArr.reduce(function (i, c) {
+			return i + c
+		});
+	};
+	
+	//sums up the total actives for an entire run
+	var totalActivesRun = function() {
+		return totalActivesBottle() * bottlesPerRun;
+	};
+	
+	//adds up values in totalServingRunArr, giving a total amount produced for the entire run
+	var totalAmtPerRun = function() {
+		return totalServingRunArr.reduce(function (i, c) {
+			return i + c
+		});
+	};
+	
+	//calculates total sum of the price for all ingredients in a run
+	var totalPriceRun = function() {
+		tempPriceArray = [];
+		for (var i = 0; i < formula.length; i++) {
+			tempPriceArray.push(priceIngredientRun(i));
+		}
+		
+		return tempPriceArray.reduce(function (i, c) {
+			return i + c
+		});
+	}
+	
+	
+	
 		//example function to add new Ingredient objects into the formula array from initial form.
-		addIngredient: function(name, amount, carrier, usdperkilo, origin, packSize) {
+		addIngredient = function(name, amount, carrier, usdperkilo, origin, packSize) {
 			var ingredient = {
 				name: name,
 				amount: amount,
@@ -26,10 +208,23 @@ var app = (function() {
 
 			formula.push(ingredient);
 		}
-	};
-})();
 
-//example ingredient
-app.carrierPerServing(); //undefined
-app.addIngredient("St. John's Wort", 500, 4, 25, manufacturer, 25);
-console.log(app.listCarrierPerServing());
+//functions used to test the program is working properly		
+addIngredient("St. John's Wort", 500, 4, 25, true, 25);
+addIngredient("5-HTP", 200, 2, 165, true, 25);
+addIngredient("Stevia", 100, 4, 35, true, 25);
+listCarrierPerServing();
+listServingTotalAmount();
+listCarrierPerBottle();
+listTotalServingPerBottle();
+listCarrierPerRun();
+listTotalPerRun();
+console.log(totalActivesRun());
+
+//This returned "undefined" but not sure why
+//var totalPriceBottle = function() {
+	//	tempPriceArray = 0;
+		//for (var i = 0; i < formula.length; i++) {
+			//total += priceIngredientBottle(i);
+		//}
+//	}
