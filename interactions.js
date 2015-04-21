f051ce52fbae5a03d22fe2d8649476797276f5e1
JonathanBechtel <JonathanBechtel@gmail.com>
2015-04-21 10:18:28 -0400

    Update interactions.js
    added comments

--- interactions.js
+++ interactions.js
@@ -1,12 +1,24 @@
+//grabs all input fields, conditional classes, and removes them
 var clean_ingredient = function(ingredient) {
   $(ingredient).find("input").val("");
   $(ingredient).find("select").val("");
   $(ingredient).find(".packSize").prop("disabled", true);
   $(ingredient).find("input").parents("td").removeClass("redClass");
   $(ingredient).find(".packSize").removeClass("required");
+  $(ingredient).find("p.message").hide();
+};
+
+//takes info from 1st row and stores it into state.runInfo
+var setRunInfo = function() {
+	var desiredPrice = parseInt($(".bottlePrice").val());
+	var containers = parseInt($(".containers").val());
+	var servings = parseInt($(".servings").val());
+			
+	manufacturingInfo(desiredPrice, containers, servings);
 };
 
 $(document).ready(function(){
+  //fn for + sign, clones tr, cleans it, attaches it to end of tbody
   $("span.glyphicon-plus-sign").click(function() {
     var ingredient = $(this).parents('.formulaRow');
     var tbody = $(this).parents('tbody');
@@ -16,7 +28,8 @@ $(document).ready(function(){
 
     $('span.glyphicon-minus-sign').show();
   });
-
+  
+  //fun for - sign, removes existing tr, sets rule for when - sign should disappear
   $("span.glyphicon-minus-sign").click(function() {
     var ingredients = $('.formulaRow');
     var ingredient = $(this).parents('.formulaRow');
@@ -26,9 +39,10 @@ $(document).ready(function(){
       $("span.glyphicon-minus-sign").hide();
   });
 
+  //fn for select dropdown, changes properties of .packSize field depending on which option is chosen
   $(".tableDropDown").on('change', function(){
     var packSize = $(this).parents('.formulaRow').find('.packSize');
-    if ($(this).val() === "me") {
+    if ($(this).val() === "false") {
       $(packSize).prop('disabled', false);
 	  $(packSize).addClass("required");
     } else {
@@ -36,46 +50,57 @@ $(document).ready(function(){
 	  $(packSize).parents('td').removeClass("redClass");
 	  $(packSize).removeClass("required");
     }
-	if ($(this).val() === "me" && $(packSize).val() === "") {
+	if ($(this).val() === "false" && $(packSize).val() === "") {
 		$(packSize).parents("td").addClass("redClass");
 	}
   });
-
-  $(".formula").click(function() {
-	$("#part2").hide();
-	$("#part1").show();
-  });
   
-  $(".ingredient, .amount, .carrier, .kilo").blur(function() {
+   //validation fn for input fields, changes color of field if left empty, shows/hides error message if value is incorrect
+  $(".ingredient, .amount, .carrier, .price").blur(function() {
 	if ($(this).val() ==="") {
 		$(this).parents("td").addClass("redClass");
 	}
 	
 	else if ($(".carrier").val() > 100) {
 		$(this).parents("td").addClass("redClass");
-		$("p.hidden").show();
+		$(this).parents("td").find("p.message").show();
 	}
 	
 	else {
 		$(this).parents("td").removeClass("redClass");
-		$("p.hidden").hide();
+		$(this).parents("td").find("p.message").hide();
 	}
   });
   
-  $(".packSize").blur(function() {
-	var tableDropDown = $(this).parents(".formulaRow").find(".tableDropDown");
-	if ($(this).val() ==="" && $(tableDropDown).val() === "me") {
-		$(this).parents("td").addClass("redClass");
+    //validation fn for first 3 inputs on page about the run - changes color if left empty
+  $(".bottlePrice, .containers, .servings").blur(function() {
+	if ($(this).val() ==="") {
+		$(this).addClass("redClass");
 	}
 	
 	else {
-		$(this).parents("td").removeClass("redClass");
+		$(this).removeClass("redClass");
 	}
+  });
+
+  //fn for 'Back to Formula' button, hides/shows divs, re-sets values of master data structure avoid duplicates
+  $(".formula").click(function() {
+	$("#part2").hide();
+	$("#part1").show();
+	state.formula = [];
+	state.servingRows = [];
+	state.bottleRows = [];
+	state.runRows = [];
+	state.sourcedIngredientRows = [];
+	state.runInfo.price = null;
+	state.runInfo.servings = null;
+	state.runInfo.containers = null;
+	$("#servingTable > tbody > tr").remove();
   });
   
   $(".analyze").click(function() {
+	//creates a counter to add up empty fields and display alert if there are any - final validation before loading table into state.
     var counter = 0;
-	var ingredient = $(".formulaRow").find("input").val();
     $(".required").each(function() {
         if ($(this).val() === "") {
             $(this).parents("td").addClass("redClass");
@@ -92,13 +117,53 @@ $(document).ready(function(){
     }
 	
 	else {
-		$(ingredient).each(function() {
-			addIngredient($(this));
-		};
-		console.log(ingredient);
+		//iterates over each tablerow, stores values into state.formula
+		$(".formulaRow").each(function() {
+			var ingredient = $(this).find(".ingredient").val();
+			var amount = parseInt($(this).find(".amount").val());
+			var carrier = parseInt($(this).find(".carrier").val());
+			var price = parseInt($(this).find(".price").val());
+			var origin = $(this).find("select").val();	
+			var packSize = parseInt($(this).find(".packSize").val());
+			
+			addIngredient(ingredient, amount, carrier, price, origin, packSize);
+			
+		});
+		
+		//run functions to put all table data into master data structure
+		setRunInfo();
+		loopSection3();
+		loopSection4();
+		loopSection5();
+		loopSourcedIngredientRows();
+		addSection3Totals();
+		addSection4Totals();
+		addSection5Totals();
+		
+		//takes each object in state.servingRows and outputs it to a new table.
+		$.each(state.servingRows, function(index, value) {
+			
+		$("#servingTable > tbody").append("<tr class='servingRow'><td>" + state.servingRows[index].ingredient + "</td><td>" + state.servingRows[index].carrier + "</td><td>" +state.servingRows[index].active + "</td><td>" + state.servingRows[index].totalAmount + "</td></tr>");
+		});
+		
+		$("#servingTable > tbody").append("<tr class='servingRow'><td><strong>Totals</strong></td><td><strong>" + state.servingTotals.carrier + "</strong></td><td><strong>" + state.servingTotals.active + "</strong></td><td><strong>" + state.servingTotals.totalAmount + "</strong></td>")
+		
 		$("#part1").hide();
 		$("#part2").show();
 	}
 });
 
 });
+
+//****The Function Graveyard - Removed due to redundancy, afraid to get rid of entirely unless they are all of a sudden needed *****//
+
+  /*$(".packSize").blur(function() { -- Silenced for now.  Want to see if just using .tableDropdown fn is enough for validation
+	var tableDropDown = $(this).parents(".formulaRow").find(".tableDropDown");
+	if ($(this).val() ==="" && $(tableDropDown).val() === "false") {
+		$(this).parents("td").addClass("redClass");
+	}
+	
+	else {
+		$(this).parents("td").removeClass("redClass");
+	}
+  }); */
