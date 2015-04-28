4cf28d939cfe845a0511cb4500649ee18883a8a5
JonathanBechtel <JonathanBechtel@gmail.com>
2015-04-28 18:59:49 -0400

    Update interactions.js

--- interactions.js
+++ interactions.js
@@ -8,6 +8,10 @@ var clean_ingredient = function(ingredient) {
   $(ingredient).find("p.message").hide();
 };
 
+var prettyNumber = function(num) {
+	$(num).number(true, 2);
+};
+
 //takes info from 1st row and stores it into state.runInfo
 var analyze = function() {
 		//iterates over each tablerow, stores values into state.formula
@@ -32,20 +36,61 @@ var analyze = function() {
 		addSection3Totals();
 		addSection4Totals();
 		addSection5Totals();
-		perBottleCostForYou();
 		
 		//takes each object in state.servingRows and outputs it to a new table.
 		$.each(state.servingRows, function(index, value) {
 			
-			$("#servingTable > tbody").append("<tr class='servingRow'><td>" + state.servingRows[index].ingredient + "</td><td>" + state.servingRows[index].carrier + "</td><td>" +state.servingRows[index].active + "</td><td>" + state.servingRows[index].totalAmount + "</td></tr>");
+			$("#servingTable > tbody").append("<tr class='servingRow'><td>" + state.servingRows[index].ingredient + "</td><td>" + state.servingRows[index].carrier + " mg</td><td>" +state.servingRows[index].active + " mg</td><td>" + state.servingRows[index].totalAmount + " mg</td></tr>");
 		});
 		
-		$("#servingTable > tbody").append("<tr class='servingRow'><td><strong>Totals</strong></td><td><strong>" + state.servingTotals.carrier + "</strong></td><td><strong>" + state.servingTotals.active + "</strong></td><td><strong>" + state.servingTotals.totalAmount + "</strong></td>")
-		$(".financeDiv#1").html("<p>  - For You: " + state.servingRows[0].ingredient + "</p><p>  - For Your Manufacturer: " + perBottleCostForManufacturer() + "</p>");
+		//adds totals to end of table
+		$("#servingTable > tbody").append("<tr class='servingTotalsRow'><td><strong>Totals</strong></td><td><strong>" + state.servingTotals.carrier + " mg</strong></td><td><strong>" + state.servingTotals.active + " mg</strong></td><td><strong>" + state.servingTotals.totalAmount + " mg</strong></td></tr>")
+		
+		//takes each object in state.bottleRows and outputs it to a new table
+		$.each(state.bottleRows, function(index, value) {
+			$("#bottleTable > tbody").append("<tr class='bottleRow'><td>" + state.bottleRows[index].ingredient + "</td><td>" + state.bottleRows[index].carrier + " mg</td><td>" + state.bottleRows[index].active + " mg</td><td>" + state.bottleRows[index].totalAmount + " mg</td><td>$" + state.bottleRows[index].price + "</td></tr>");
+		});
+		
+		//adds totals to end of table
+		$("#bottleTable > tbody").append("<tr class='bottleTotalsRow'><td><strong>Totals</strong></td><td><strong>" + state.bottleTotals.carrier + " mg</strong></td><td><strong>" + state.bottleTotals.active + " mg</strong></td><td><strong>" + state.bottleTotals.totalAmount + " mg</strong></td><td><strong>$" + state.bottleTotals.price +  "</strong></td></tr>")
+		
+		//takes each object in state.runRows and outputs it to a new table
+		$.each(state.runRows, function(index, value) {
+			$("#runTable > tbody").append("<tr class='runRow'><td>" + state.runRows[index].ingredient + "</td><td>" + state.runRows[index].carrier + " kg</td><td>" + state.runRows[index].active + " kg</td><td>" + state.runRows[index].totalAmount + " kg</td><td>$" + state.runRows[index].price + "</td></tr>");
+			$(data.addRows).push([state.runRows[index].ingredient, state.runRows[index].totalAmount]);
+		});
+		
+		//adds totals to end of table
+		$("#runTable > tbody").append("<tr class='runTotalsRow'><td><strong>Totals</strong></td><td><strong>" + state.runTotals.carrier + " kg</strong></td><td><strong>" + state.runTotals.active + " kg</strong></td><td><strong>" + state.runTotals.totalAmount + " kg</strong></td><td><strong>$" + state.runTotals.price +  "</strong></td></tr>")
+		
+		$(".financeDiv1").html("<p class='text-center'>  For You: $" + perBottleCostForYou() + "</p><p class='text-center'>  For Your Manufacturer: $" + perBottleCostForManufacturer() + "</p>");
+		
+		$(".financeDiv2").html("<p class='text-center'>  For You: $" + totalIngredientCostForYou() + "</p><p class='text-center'>  For Your Manufacturer: $" + totalIngredientCostForManufacturer() + " </p>");
+		
+		$(".financeDiv3").html("<p class='text-center'>  $" + estimatedQuotePrice() + "</p>");
+		
+		if (state.sourcedIngredientRows.length > 0) {
+			$.each(state.sourcedIngredientRows, function(index, value) {
+				$("#sourcedTable > tbody").append("<tr class='sourcedRow'><td>" + state.sourcedIngredientRows[index].ingredient + "</td><td>$" + state.sourcedIngredientRows[index].costPerPack + "</td><td>" + state.sourcedIngredientRows[index].packsRequired  + "</td><td>$" + state.sourcedIngredientRows[index].costForAllPacks + "</td><td>" + (state.sourcedIngredientRows[index].percentUsed)*100 + "%</td><td>" + state.sourcedIngredientRows[index].amountLeftOver + " kg</td><td>" + state.sourcedIngredientRows[index].bottlesRemaining + "</td></tr>")
+			});
+			
+			$(".sourcedIngredientsRow").show();
+		}
+		
+		else {
+			$(".sourcedIngredientsRow").hide();
+		}
+		
+		$(".financeDiv4").html("<p class='text-center'>$" + amtPaidToMfctr() + " </p>");
+		
+		$(".financeDiv5").html("<p class='text-center'>$" + upfrontIngredientCost() + "</p>");
+		
+		$(".financeDiv6").html("<p class='text-center'>$" + totalUpfrontCosts() + "</p>");
 		
 		$("#part1").hide();
 		$("#part2").show();
 }
+
 var setRunInfo = function() {
 	var desiredPrice = parseInt($(".bottlePrice").val());
 	var containers = parseInt($(".containers").val());
@@ -66,6 +111,10 @@ $(document).ready(function(){
 
     $('span.glyphicon-minus-sign').show();
   });
+  
+  $(function () {
+  $('[data-toggle="popover"]').popover();
+	});
   
   //fun for - sign, removes existing tr, sets rule for when - sign should disappear
   $("span.glyphicon-minus-sign").click(function() {
@@ -90,6 +139,18 @@ $(document).ready(function(){
     }
 	if ($(this).val() === "false" && $(packSize).val() === "") {
 		$(packSize).parents("td").addClass("redClass");
+	}
+  });
+  
+  //To remove redClass when a user leaves packSize input
+  $(".packSize").blur(function() {
+	var tableDropDown = $(this).parents(".formulaRow").find(".tableDropDown");
+	if ($(this).val() ==="" && $(tableDropDown).val() === "false") {
+		$(this).parents("td").addClass("redClass");
+	}
+	
+	else {
+		$(this).parents("td").removeClass("redClass");
 	}
   });
   
@@ -134,6 +195,9 @@ $(document).ready(function(){
 	state.runInfo.servings = null;
 	state.runInfo.containers = null;
 	$("#servingTable > tbody > tr").remove();
+	$("#bottleTable > tbody > tr").remove();
+	$("#runTable > tbody > tr").remove();
+	$("#sourcedTable > tbody > tr").remove();
   });
   
   $(".analyze").click(function() {
@@ -162,14 +226,3 @@ $(document).ready(function(){
 });
 
 //****The Function Graveyard - Removed due to redundancy, afraid to get rid of entirely unless they are all of a sudden needed *****//
-
-  /*$(".packSize").blur(function() { -- Silenced for now.  Want to see if just using .tableDropdown fn is enough for validation
-	var tableDropDown = $(this).parents(".formulaRow").find(".tableDropDown");
-	if ($(this).val() ==="" && $(tableDropDown).val() === "false") {
-		$(this).parents("td").addClass("redClass");
-	}
-	
-	else {
-		$(this).parents("td").removeClass("redClass");
-	}
-  }); */
